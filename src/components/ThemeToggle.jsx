f456c08657df07ac84457c6c3f140212e76cab55
Pratik Shinde <shindepratik582004@gmail.com>
2025-11-23 23:12:54 +0700

    change default color theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,15 +6,25 @@ export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
+  const storedTheme = localStorage.getItem("theme");
+
+  if (storedTheme) {
+    // If user has saved a theme
     if (storedTheme === "dark") {
-      setIsDarkMode(true);
       document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
     } else {
-      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
       setIsDarkMode(false);
     }
+  } else {
+    // Default theme = DARK
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    setIsDarkMode(true);
+  }
   }, []);
+
 
   const toggleTheme = () => {
     if (isDarkMode) {
